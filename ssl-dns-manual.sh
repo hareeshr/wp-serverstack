73401c6ea5ffb11a15f793595cc6e141ad558c0f
hareeshr <hareeshr.2007@gmail.com>
2018-05-24 11:49:03 +0530

    origin added

--- ssl-dns-manual.sh
+++ ssl-dns-manual.sh
@@ -1,7 +1,7 @@
 ########## Issue Key by DNS Challenge #################
 
 # change example.com to your domain and tools to your admin subdomain
-certbot -d example.com -d www.example.com -d tools.example.com --manual --preferred-challenges dns certonly
+certbot -d example.com -d www.example.com -d tools.example.com -d origin.example.com --manual --preferred-challenges dns certonly
 # give TXT record for all three domains with given name and value pair
 
 #if dhparam.pem file not created run this
--- ssl-dns.sh
+++ ssl-dns.sh
@@ -4,12 +4,14 @@
 WEBSITE="example.com"
 #default admin subwebsite
 ADMINSD="tools"
+#default origin subwebsite
+ORIGINSD="origin"
 
 
 ########## Issue Key by DNS Challenge #################
 
 # DNS Challenge
-certbot -d $WEBSITE -d www.$WEBSITE -d $ADMINSD.$WEBSITE --manual --preferred-challenges dns certonly
+certbot -d $WEBSITE -d www.$WEBSITE -d $ADMINSD.$WEBSITE -d $ORIGINSD.$WEBSITE --manual --preferred-challenges dns certonly
 # give TXT record for all three domains with given name and value pair
 
 if [ ! -f /etc/ssl/certs/dhparam.pem ]; then
